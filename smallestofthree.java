2fbf7be811541a8bb035d4110b392c79119a9f56
Rohitydv025 <155579514+Rohitydv025@users.noreply.github.com>
2024-09-28 00:30:06 +0530

    Update smallestofthree.java

--- smallestofthree.java
+++ smallestofthree.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
-
-
- public class smallestofthree {
+public class smallestofthree {
      public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
          // the least number between three
