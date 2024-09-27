b3a81fa0017708a1f6740805c0be5b64b850a8cd
Rohitydv025 <155579514+Rohitydv025@users.noreply.github.com>
2024-09-28 00:27:50 +0530

    Add files via upload

---
+++ greatest_3numbers.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+
+public class greatest_3numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println( " the gratest of three numbers ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        int greatest ;
+        if ( num1 >= num2 && num1 >= num3 ){
+            greatest = num1 ;
+
+        } else if (num2 >= num1 && num2 >= num3 ) {
+            greatest= num2 ;
+        }
+        else {
+            greatest = num3;
+        }
+
+        //output
+       System.out.println( " the gretest is "+greatest);
+
+
+
+
+
+
+
+    }
+
+
+
+
+    }
+
+
+
---
+++ smallestofthree.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+ public class smallestofthree {
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         // the least number between three
+         System.out.println( " Enter the first number  ");
+         int a = sc.nextInt( );
+         System.out.println( " Enter the second number  ");
+         int b = sc.nextInt( );
+         System.out.println( " Enter the third number  ");
+         int c = sc.nextInt( );
+         if( a <= b && a<= c ){
+             System.out.println( a+ " is least number ");
+         }
+         else if(b <= a && b<= c ){
+             System.out.println( b+ " is least number ");
+         }
+         else  {
+             System.out.println(c + " is least");
+         }
+     }
+}
