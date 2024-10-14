08324e9bf5fd87e9df75faaff1f96aeb12146752
Rohitydv025 <155579514+Rohitydv025@users.noreply.github.com>
2024-10-14 12:59:06 +0530

    Add files via upload

---
+++ star_triangle.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Scanner;
+public class star_triangle{
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print( " enter the no of rows and columns:   " );
+        int n = sc.nextInt();
+        for(int i = 1 ; i <= n ;i++){
+            for (int j=1; j <=i;j++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+}
+
+
+
